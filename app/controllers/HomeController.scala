8cb4a8a4973a4314f29dedd7c0f26bb391711c07
Alexander Keller <keller.ar98@gmail.com>
2021-10-18 16:32:40 -0700

    Scala Project Import V_2 + simple HTTP Requests

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import dog._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -10,6 +11,8 @@ import play.api.mvc._
  */
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  var gameController = Dog.controller
 
   /**
    * Create an Action to render an HTML page.
@@ -20,5 +23,33 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
+  }
+
+   def house() = Action {
+    Ok("house" + gameController.toStringHouse)
+  }
+  def load() = Action {
+    Ok("load" + gameController.load)
+  }
+  def save() = Action {
+    Ok("save" + gameController.save())
+  }
+  def undoCommand() = Action {
+    Ok("undo" + gameController.undoCommand())
+  }
+
+  def redoCommand() = Action {
+    Ok("redo" + gameController.redoCommand())
+  }
+
+  def doStep() = Action {
+    Ok("doStep" + gameController.doStep())
+  }
+
+  def createNewBoard(size: Int) = Action {
+    Ok("createNewBoard" + gameController.createNewBoard(size))
+  }
+  def playerhands() = Action {
+    Ok("playerhands" + gameController.toStringPlayerHands)
   }
 }
