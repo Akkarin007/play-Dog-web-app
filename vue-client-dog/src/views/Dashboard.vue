650f95a1d9799059da6f6c0509ce97749825ad1c
Ismail Zeybek <isyzeybek@outlook.com>
2021-12-06 16:32:59 +0100

    first init Vue and Play

---
+++ vue-client-dog/src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <game-dashboard />
+</template>
+
+<script lang="ts">
+import GameDashboard from '@/components/GameDashboard.vue'
+import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'Home',
+
+    components: {
+      GameDashboard
+    },
+  })
+</script>
