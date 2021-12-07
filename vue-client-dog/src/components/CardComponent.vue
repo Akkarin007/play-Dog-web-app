742b4a4babac5c72de297c41205d1a6e7b987e31
Ismail Zeybek <isyzeybek@outlook.com>
2021-12-07 09:57:17 +0100

    ui cleanup

--- vue-client-dog/src/components/CardComponent.vue
+++ vue-client-dog/src/components/CardComponent.vue
@@ -3,23 +3,24 @@
     
     <v-card
     class="mx-auto"
-    max-width="160"
+    max-width="150"
     rounded
   >
     <div class="justify-center d-flex">
+
     <img 
       src="../assets/images/cards/1 11 play.png"
       height="200px"
     >
-
     </div>
-
-    <v-btn class="ma-3"
-              small
-              dark
-            >
-              play
-            </v-btn>
+    <div class="justify-center d-flex">
+      <v-btn class="ma-3"
+        small
+        dark
+      >
+        play
+      </v-btn>
+    </div>
 
   </v-card>
   </v-container>
