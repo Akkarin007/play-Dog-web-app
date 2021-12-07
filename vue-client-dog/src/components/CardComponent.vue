78169415fc4ebef93b66a84062612644802f81cf
Ismail Zeybek <isyzeybek@outlook.com>
2021-12-07 01:26:45 +0100

    added basic ui components

---
+++ vue-client-dog/src/components/CardComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container>
+    
+    <v-card
+    class="mx-auto"
+    max-width="160"
+    rounded
+  >
+    <div class="justify-center d-flex">
+    <img 
+      src="../assets/images/cards/1 11 play.png"
+      height="200px"
+    >
+
+    </div>
+
+    <v-btn class="ma-3"
+              small
+              dark
+            >
+              play
+            </v-btn>
+
+  </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+
+import Vue from "vue";
+import board,{field} from "../assets/data/boardExample";
+
+export default Vue.extend({
+  name: "CardComponent",
+
+  data: () => ({
+    image: "../assets/images/icons/field.png",
+    board,
+    field: field()
+  }),
+  computed: {
+  
+  }
+});
+
+</script>
--- vue-client-dog/src/components/GameDashboard.vue
+++ vue-client-dog/src/components/GameDashboard.vue
@@ -3,24 +3,75 @@
     <v-row>
       <v-col cols="12" sm="2">
         <v-sheet rounded="lg" min-height="268">
-          <!--  -->
+          <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>CurrentPlayer: P{{board.currentPlayer}}</v-list-item-title>
+            <v-list-item-subtitle>{{currentPlayerHouse}} Pieces Left to Play!</v-list-item-subtitle>
+             <v-row cols="12" sm="8" class="ma-2">
+            <img v-for="img in inHouse" :key="img.id" 
+                    :src="img.image"
+                    loading="lazy"
+                    width="50"
+                    height="50"
+                  />
+                  </v-row>
+          </v-list-item-content>
+        </v-list-item>
         </v-sheet>
       </v-col>
 
       <v-col cols="12" sm="8">
-        <v-sheet min-height="70vh" rounded="lg">
-
-   
-          <v-btn class="mx-2" fab  small  >
-            <img src="../assets/images/icons/blau.png" loading="lazy" width="50" height="50">
-          </v-btn>
+        <v-sheet
+          min-height="100vh"
+          class="d-flex overflow-auto justify-center"
+          rounded="lg"
+        >
+          <v-col cols="12" sm="20">
+            <div class="ma-2 d-flex overflow-auto justify-center">
+              <h1>Welcome to Dog!</h1>
+            </div>
+            <v-row cols="12" sm="8" class="ma-2">
+              <div v-for="img in field" :key="img.pos">
+                <v-btn class="ma-2" fab small>
+                  <img
+                    :id="img.pos"
+                    :src="img.image"
+                    loading="lazy"
+                    width="50"
+                    height="50"
+                  />
+                </v-btn>
+              </div>
+            </v-row>
+            <div>
+              <div class="d-flex overflow-auto">
+                <div v-for="img in field" :key="img.pos">
+                  <card-component></card-component>
+                </div>
+              </div>
+            </div>
+          </v-col>
           <!--  -->
         </v-sheet>
       </v-col>
 
       <v-col cols="12" sm="2">
         <v-sheet rounded="lg" min-height="268">
-          <!--  -->
+          <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title>Players Garage </v-list-item-title>
+            <v-list-item-subtitle>...</v-list-item-subtitle>
+             <v-row cols="12" sm="8" class="ma-2">
+            <img v-for="img in garage" :key="img.id" 
+                    :src="img.image"
+                    loading="lazy"
+                    width="50"
+                    height="50"
+                  />
+                  </v-row>
+          </v-list-item-content>
+        </v-list-item>
+        
         </v-sheet>
       </v-col>
     </v-row>
@@ -28,46 +79,28 @@
 </template>
 
 <script lang="ts">
-
 import Vue from "vue";
-import board from "../assets/data/boardExample";
+import board, { field, inhouse, garage } from "../assets/data/boardExample";
+import CardComponent from "./CardComponent.vue";
 
 export default Vue.extend({
+  components: { CardComponent },
   name: "GameDashboard",
 
   data: () => ({
     board,
-    field: [] }),
+    currentPlayerHouse: board.players[board.currentPlayer].house.length
+  }),
   computed: {
-    board(){
-      const boardSize = this.board.boardSize;
-      const pieceArray = this.board.players.map(player => {
-     
-      const pieces = player.pieces.map(piece => {
-          return { pieceIdx: piece.piece_idx, piecePos: piece.piece_pos}
-        }).filter(piece => player.house.find(inHousePieceIdx => piece.pieceIdx == inHousePieceIdx.inHouse));
-
-        return {color: player.color, pieces: pieces}
-      })
-
-      const boardArray = []
-
-      for (let i = 0; i < boardSize; i++) {
-        for (let playerIdx = 0; playerIdx < this.board.playerNumber; playerIdx++) {
-          const element = pieceArray[playerIdx].pieces.find(piece => piece.piecePos === i);
-          if (element){
-            boardArray.push({color: pieceArray[playerIdx].color, pos: i})
-          }
-          else {
-            boardArray.push({color: null, pos: i})
-          }
-          
-        }
-      }
-
-      return boardArray
-    }
-  }
+    field() {
+      return field();
+    },
+    inHouse() {
+      return inhouse();
+    },
+    garage() {
+      return garage();
+    },
+  },
 });
-
 </script>
