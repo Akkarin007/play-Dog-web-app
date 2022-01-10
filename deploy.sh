376cf983d6e120f7aa5d1539d3eebcb788644b8f
Ismail Zeybek <isyzeybek@outlook.com>
2022-01-10 16:29:14 +0100

    fix deploy sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add *
 git commit -m "heroku deployment-gitpages"
-git push -f git@github.com:Akkarin007/play-Dog-web-app.git WA-12-deployment-vue:gh-pages
+git push -f git@github.com:Akkarin007/play-Dog-web-app.git master:gh-pages
 
 cd -
