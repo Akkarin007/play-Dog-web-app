2af97a4d40134cd6c64e4ffef29a276ceebf0543
Ismail Zeybek <isyzeybek@outlook.com>
2022-01-25 00:36:53 +0100

    heroku deployment-gitpages

---
+++ precache-manifest.acf892470e98cd645573da39769f534f.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ad7545bf237c74549a",
+    "url": "/play-Dog-web-app/css/about.df9d9a7c.css"
+  },
+  {
+    "revision": "7ebb5818d7eb9a38916e",
+    "url": "/play-Dog-web-app/css/chunk-vendors.72e475a3.css"
+  },
+  {
+    "revision": "f37d608fb18056bc4f83737d633c5cf5",
+    "url": "/play-Dog-web-app/img/1 11 play.f37d608f.png"
+  },
+  {
+    "revision": "b208cd7f21ce87cf950936ab6fb3e717",
+    "url": "/play-Dog-web-app/img/10.b208cd7f.png"
+  },
+  {
+    "revision": "1963502749c024e1590789bab3632bc9",
+    "url": "/play-Dog-web-app/img/10C.19635027.png"
+  },
+  {
+    "revision": "60ab387c8c1c50dd3a75255500218b94",
+    "url": "/play-Dog-web-app/img/10H.60ab387c.png"
+  },
+  {
+    "revision": "9026849bcbad6fd92f7c3cb8f70e3cd4",
+    "url": "/play-Dog-web-app/img/12.9026849b.png"
+  },
+  {
+    "revision": "17ca9f77b395a41c6d45cbff350573bb",
+    "url": "/play-Dog-web-app/img/13 play.17ca9f77.png"
+  },
+  {
+    "revision": "33c964ae685ff65233d2ac2c35b96072",
+    "url": "/play-Dog-web-app/img/2.33c964ae.png"
+  },
+  {
+    "revision": "136dab439961d61f4b9c1f939e07828d",
+    "url": "/play-Dog-web-app/img/2C.136dab43.png"
+  },
+  {
+    "revision": "ebe434953cb5c82ec5c8e38b8e735489",
+    "url": "/play-Dog-web-app/img/2H.ebe43495.png"
+  },
+  {
+    "revision": "9638bf9f29919b027765992a81f6d39b",
+    "url": "/play-Dog-web-app/img/3.9638bf9f.png"
+  },
+  {
+    "revision": "6ed5bf64f008d72e83c67f81468ab7c5",
+    "url": "/play-Dog-web-app/img/3C.6ed5bf64.png"
+  },
+  {
+    "revision": "7278d51dda50bd4b8c8cadb7fe349953",
+    "url": "/play-Dog-web-app/img/3H.7278d51d.png"
+  },
+  {
+    "revision": "6695f1d514765572dfe7e5dd0863dfce",
+    "url": "/play-Dog-web-app/img/4.6695f1d5.png"
+  },
+  {
+    "revision": "23c51e0b449e342f8b7f6c8fd57e2137",
+    "url": "/play-Dog-web-app/img/4C.23c51e0b.png"
+  },
+  {
+    "revision": "89eaac32133444fe40876cb51f697f13",
+    "url": "/play-Dog-web-app/img/4D.89eaac32.png"
+  },
+  {
+    "revision": "5156259e1f30dece1376dc5695a9a1d4",
+    "url": "/play-Dog-web-app/img/4S.5156259e.png"
+  },
+  {
+    "revision": "5ab3d8627d0c4d17c86c73d90817900b",
+    "url": "/play-Dog-web-app/img/5.5ab3d862.png"
+  },
+  {
+    "revision": "3ac88eddd1ac03bfc901de76424b5aba",
+    "url": "/play-Dog-web-app/img/5C.3ac88edd.png"
+  },
+  {
+    "revision": "5457f189cabc0476511c7ed1421b419d",
+    "url": "/play-Dog-web-app/img/5H.5457f189.png"
+  },
+  {
+    "revision": "c99b0f04000841e5f3db53d64b3f7034",
+    "url": "/play-Dog-web-app/img/6.c99b0f04.png"
+  },
+  {
+    "revision": "5c96f40f637a6b2ed34d5e837d81ef7b",
+    "url": "/play-Dog-web-app/img/6C.5c96f40f.png"
+  },
+  {
+    "revision": "84564e31bf0a0e8bcd5fdd3d4aa12cc3",
+    "url": "/play-Dog-web-app/img/6H.84564e31.png"
+  },
+  {
+    "revision": "82d62864ff67ff7b5e20150316d26872",
+    "url": "/play-Dog-web-app/img/7.82d62864.png"
+  },
+  {
+    "revision": "4808cc416c976dbd5c3cb629a0f102a5",
+    "url": "/play-Dog-web-app/img/7C.4808cc41.png"
+  },
+  {
+    "revision": "58d872f063752027e59e15af4d090123",
+    "url": "/play-Dog-web-app/img/7D.58d872f0.png"
+  },
+  {
+    "revision": "497eed1efa7e554de5b62bc9eb2b5ae6",
+    "url": "/play-Dog-web-app/img/8.497eed1e.png"
+  },
+  {
+    "revision": "a46a54d93ff6e543497d205db97fd460",
+    "url": "/play-Dog-web-app/img/8C.a46a54d9.png"
+  },
+  {
+    "revision": "1e9e59b0d6f684908f3ad7d2fe942a42",
+    "url": "/play-Dog-web-app/img/8H.1e9e59b0.png"
+  },
+  {
+    "revision": "50caac183ef66afe6cdda1d2e3daafda",
+    "url": "/play-Dog-web-app/img/9.50caac18.png"
+  },
+  {
+    "revision": "4f8d0d8f2ebaeacf99e7134221d6da49",
+    "url": "/play-Dog-web-app/img/9C.4f8d0d8f.png"
+  },
+  {
+    "revision": "33ab7891b4259ea8d8823a352ef38cdf",
+    "url": "/play-Dog-web-app/img/9H.33ab7891.png"
+  },
+  {
+    "revision": "1595a3ea21f452c290cf26af7ae53ede",
+    "url": "/play-Dog-web-app/img/JC.1595a3ea.png"
+  },
+  {
+    "revision": "d5682eb454510b9cdf54beb9c5a0e5f5",
+    "url": "/play-Dog-web-app/img/JD.d5682eb4.png"
+  },
+  {
+    "revision": "fdf4ffc434ac567c6b77905cb1b11c8a",
+    "url": "/play-Dog-web-app/img/JH.fdf4ffc4.png"
+  },
+  {
+    "revision": "89d2c97e62fcaec36c80dbdd725f216f",
+    "url": "/play-Dog-web-app/img/JS.89d2c97e.png"
+  },
+  {
+    "revision": "aa73cfd0e548d87f4af4a18a8d2c3c8f",
+    "url": "/play-Dog-web-app/img/KC.aa73cfd0.png"
+  },
+  {
+    "revision": "3b48ae8207b07a1f9477d7e6135ca211",
+    "url": "/play-Dog-web-app/img/KH.3b48ae82.png"
+  },
+  {
+    "revision": "d6296277472d7e9594d0ebee0cb82cdc",
+    "url": "/play-Dog-web-app/img/QC.d6296277.png"
+  },
+  {
+    "revision": "6940967da8669e3410fefd3a8a33aad9",
+    "url": "/play-Dog-web-app/img/QD.6940967d.png"
+  },
+  {
+    "revision": "63afa62b47efa84b8b87d3e39ef42bc1",
+    "url": "/play-Dog-web-app/img/QH.63afa62b.png"
+  },
+  {
+    "revision": "80d296e9d8cb90b9802fad8d63b71dc5",
+    "url": "/play-Dog-web-app/img/green_back.80d296e9.png"
+  },
+  {
+    "revision": "2fe506725db288239599e8ca351c4b91",
+    "url": "/play-Dog-web-app/img/laidcarddeck.2fe50672.png"
+  },
+  {
+    "revision": "1a1a081b51d2f3db48b6ff366339dd54",
+    "url": "/play-Dog-web-app/img/playbutton.1a1a081b.png"
+  },
+  {
+    "revision": "a762540d55018bcf43ffba8b6343c9c9",
+    "url": "/play-Dog-web-app/img/questionmark.a762540d.png"
+  },
+  {
+    "revision": "e6a6d2f8a350079dcad97e814d3559e7",
+    "url": "/play-Dog-web-app/img/swapCard.e6a6d2f8.png"
+  },
+  {
+    "revision": "61f4d69bd15248d6a4722b05e2790f64",
+    "url": "/play-Dog-web-app/index.html"
+  },
+  {
+    "revision": "42ad7545bf237c74549a",
+    "url": "/play-Dog-web-app/js/about.f904be83.js"
+  },
+  {
+    "revision": "5b3f6ac774bce69a5643",
+    "url": "/play-Dog-web-app/js/app.0bf30349.js"
+  },
+  {
+    "revision": "7ebb5818d7eb9a38916e",
+    "url": "/play-Dog-web-app/js/chunk-vendors.f5ddbc09.js"
+  },
+  {
+    "revision": "4dd560519a84171f166db110d6e96c9b",
+    "url": "/play-Dog-web-app/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/play-Dog-web-app/robots.txt"
+  }
+]);
